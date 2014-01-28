3ab67f1df61b8de8af14a6075249ab4a2295e17b
Georgi Georgiev <georgiev.georgio@gmail.com>
2014-01-28 12:45:54 +0000

    refractoring for twitter sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+	def nag_for_email
+		     if user_singed_in? && current_user.email.blank?
+		flash[:error] = 'Please fill in your email'
+				 end
+	end
+
 end
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,13 +3,17 @@ class OmniauthCallbacksController < ApplicationController
 	def twitter
 		    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      # set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    #twitter's devise documentation which we shortened
+
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    #   # set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    # else
+    #   session["devise.twitter_data"] = request.env["omniauth.auth"]
+    #   redirect_to new_user_registration_url
+    # end
 	end
 end
+
